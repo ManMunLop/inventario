fd669a846929bc5bf90b543114fc1fe1da393c64
ManMunLop <man.munlop@iesmai.es>
2026-04-30 08:52:02 +0200

    Cambios por el front

--- src/main/java/ies_alcores/api_inv/controller/ProductoController.java
+++ src/main/java/ies_alcores/api_inv/controller/ProductoController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 @RestController
 @RequestMapping("/api/inv")
-@CrossOrigin(origins = "http://localhost:4200") // IMPORTANTE: Esto permite que Angular se conecte
+@CrossOrigin(origins = "http://localhost:4200")
 public class ProductoController {
 
     @Autowired
